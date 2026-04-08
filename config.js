89c1d6bfba1c203f4df4749a8a5e0476c6b6ff79
2026musik-code <2026musik@gmail.com>
2026-04-08 16:39:09 +0700

    feat: Update worker URL for payments
    The WORKER_URL in the config has been updated to a new endpoint. This change ensures that payment processing is routed through the correct worker.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Konfigurasi URL Worker untuk pembayaran
 window.APP_CONFIG = {
     // Ganti URL di bawah ini dengan URL Worker yang Anda dapatkan setelah deploy
-    WORKER_URL: 'https://paymenku-proxy.username.workers.dev/' 
+    WORKER_URL: 'https://abot.ahem7553.workers.dev/' 
 };
